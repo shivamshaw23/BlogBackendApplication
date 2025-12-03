f5001515dcef663be876f02d0980b13df785f99d
shivamshaw23 <154890864+shivamshaw23@users.noreply.github.com>
2025-12-03 14:00:53 +0530

    Delete Blog application Backend directory

--- Blog application Backend/src/main/java/com/backend/blog/repositories/CommentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.backend.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.backend.blog.entities.Comment;
-
-public interface CommentRepo  extends JpaRepository<Comment	, Integer> {
-
-}
